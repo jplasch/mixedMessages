59896c219c6b09978a256edd0d17a533065e165d
pyropulse137 <64796650+jplasch@users.noreply.github.com>
2025-02-12 17:49:59 -0800

    Add messages to script.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is my first project
+========================
+
+- Generate random horoscope each time it is ran
+- Contains three distinct message aspects that will be combined into a coherant, single message
+- Will soon add ASCII art!
--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('test');
+// Initialize message array
+const planetArr = ['Moon', 'Mercury', 'Venus', 'Sun', 'Mars', 'Jupiter', 'Saturn'];
+const zodiacArr = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+const fortuneArr = ['great fortune', 'great misfortune', 'impending doom', 'blossoming love'];
