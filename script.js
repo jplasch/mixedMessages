33013583e8c2eb0b99a1ec8db4e3e29350793c6e
pyropulse137 <64796650+jplasch@users.noreply.github.com>
2025-02-12 18:51:34 -0800

    Add random message functionality to script.js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 // Initialize message array
-const planetArr = ['Moon', 'Mercury', 'Venus', 'Sun', 'Mars', 'Jupiter', 'Saturn'];
+const planetArr = ['Luna', 'Mercury', 'Venus', 'Sol', 'Mars', 'Jupiter', 'Saturn'];
 const zodiacArr = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
 const fortuneArr = ['great fortune', 'great misfortune', 'impending doom', 'blossoming love'];
+
+// Store in object for easy access
+const msgObj = {
+    planet: planetArr,
+    zodiac: zodiacArr,
+    fortune: fortuneArr,
+};
+
+// Creates the random message
+const createMessage = () => {
+    for (const prop in msgObj) {
+        const randNum = Math.floor(Math.random() * msgObj[prop].length);
+        switch (prop) {
+            case 'planet':
+                console.log(`${msgObj[prop][randNum]} rules your desinty...`);
+                break;
+            case 'zodiac':
+                console.log(`Whilst the sign of ${msgObj[prop][randNum]} severs the threads of fate;`);
+                break;
+            case 'fortune':
+                console.log(`Thus, you will experience ${msgObj[prop][randNum]}.`);
+                break;
+        }
+    }
+}
+
+// Create random message
+createMessage();
